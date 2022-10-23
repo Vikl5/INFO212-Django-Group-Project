85c51906d9951775292a740837789702ee055879
Vikl5 <Vikl5@protonmail.com>
2022-10-23 22:50:28 +0200

    Added a models.py
    Contains the car model and make of the car

---
+++ django-env/dealership/dealership/models.py
@@ -0,0 +1,9 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+class Car(models.Model):
+    make = models.CharField(max_length = 50)
+    carmodel = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.make + '' + self.carmodel
