6f77d241433c9396de7cc732c6fb5f0fed332b4d
Vikl5 <Vikl5@protonmail.com>
2022-10-24 13:48:18 +0200

    Added operable method to framework
    You can now update, save or delete a car

--- django-env/dealership/dealership/views.py
+++ django-env/dealership/dealership/views.py
@@ -5,8 +5,44 @@ from rest_framework import status
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
+#Following code is taken from the Lecture notes in INFO212
+#LECTURE 08: ARCHITECTURAL SOLUTIONS - Fazle  Rabbi
+#https://mitt.uib.no/courses/35975/files?preview=4543975
+
 @api_view(['GET'])
 def get_cars(request):
     cars = Car.objects.all()
     serializer = CarSerializer(cars, many=True)
+    print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def save_car(request):
+    serializer = CarSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT'])
+def update_car(request, id):
+    try:
+        theCar = Car.objects.get(pk=id)
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = CarSerializer(theCar, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['DELETE'])
+def delete_car(request, id):
+    try:
+        theCar = Car.objects.get(pk=id)
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    theCar.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+    
