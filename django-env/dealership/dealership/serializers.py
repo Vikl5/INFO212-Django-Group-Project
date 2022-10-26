7a603cd42eac2aadc693b757d71399fd0ac69722
Vikl5 <Vikl5@protonmail.com>
2022-10-26 12:11:19 +0200

    Created Customer serializer

--- django-env/dealership/dealership/serializers.py
+++ django-env/dealership/dealership/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from .models import Car
+from .models import Car, Customer
+
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'make', 'carmodel']
+        fields = ['id', 'make', 'carmodel', 'year', 'location', 'status']
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'name', 'age', 'address']
