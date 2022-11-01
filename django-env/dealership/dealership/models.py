ca8365a802a9e541c23c3b0abdbe715de30e5116
Vikl5 <Vikl5@protonmail.com>
2022-11-01 20:53:13 +0100

    Added one-to-one relation

--- django-env/dealership/dealership/models.py
+++ django-env/dealership/dealership/models.py
@@ -1,13 +1,13 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
+
 class Car(models.Model):
     make = models.CharField(max_length = 50)
     carmodel = models.CharField(max_length = 50)
     year = models.CharField(max_length = 50)
     location = models.CharField(max_length = 50)
     status = models.CharField(max_length = 50)
-    
 
     def __str__(self):
         return self.make + ' ' + self.carmodel + ' ' + self.year + ' ' + self.location + ' ' + self.status
@@ -16,6 +16,8 @@ class Customer(models.Model):
     name = models.CharField(max_length = 200)
     age = models.CharField(max_length = 20)
     address = models.CharField(max_length = 200)
+    customer_booking = models.OneToOneField(Car, on_delete=models.SET_NULL, null=True)
+    #booked = models.CharField(max_length = 50)
 
     def __str__(self):
         return self.name + ' ' + self.age + ' ' + self.address
